75777e90645115aadeb91b418a705b6725f4fcea
Jay <jaytat132@gmail.com>
2024-06-25 21:25:15 -0700

    rounded the duck

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,7 +10,7 @@ const Photo = () => {
             initial ={{opacity: 0}} 
             animate= {{opacity:1, transition: {delay: 2, duration: 0.4, ease: "easeIn"}}}
                 className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighte">
-                <Image src="/assets/duck.png" 
+                <Image src="/assets/duck-modified.png" 
                 priority 
                 quality={100} 
                 fill 
