645a80ce8620ba6cc3d46fd3b1c4e3a8dae4acc8
Jay <jaytat132@gmail.com>
2024-06-25 20:24:23 -0700

    fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,17 @@
 import { JetBrains_Mono } from "next/font/google";
-import "./globals.css"; 
+import "./globals.css";
 
 // components
-import Header from "@/components/Header"
+import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ["100","200","300","400","500","600","700","800"],
-  variable: '--found-jetbrainsMono'
- });
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--found-jetbrainsMono",
+});
 
 export const metadata = {
   title: "Jay Tat",
@@ -21,10 +23,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        <Analytics/>
-        <SpeedInsights/>
-        <PageTransition>{children}</PageTransition>
-        </body>
+        <PageTransition>
+          <Analytics />
+          <SpeedInsights />
+          {children}
+        </PageTransition>
+      </body>
     </html>
   );
 }
