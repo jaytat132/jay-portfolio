29ad37ff042b59729bae0f1e5c37dd74a7589315
Jay <jaytat132@gmail.com>
2024-06-26 20:33:24 -0700

    moved image to the right.

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
-import NameAnimation from "@/components/NameAnimation"
-import Photo from "@/components/Photo"
-import Social from "@/components/Social"
-
+import NameAnimation from "@/components/NameAnimation";
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
 
 const Home = () => {
   return (
@@ -10,28 +9,28 @@ const Home = () => {
         <div className="flex flex-col items-center justify-center text-center xl:flex-row xl:items-center xl:justify-between xl:pt-8 xl:pb-24 xl:text-left">
           {/* text */}
           <div>
-            <h1 className="h1">
-              Hello, my name is
+            <h1 className="h1 leading-tight">
+              Hello, my<br />
+              name is
             </h1>
-            <br /> 
             <NameAnimation />
-             {/* Empty Line*/}
-             <div className="my-4"></div>
-            <p>I'm currently an upcoming Senior studying Computer Science at the University of California, Davis.</p>
-            {/* Empty Line*/}
-            <div className="my-4"></div>
-            {/*socials*/}
-            <div className="mb-8 xl:mb-0">
-              <Social containerStyles="flex gap-6 justify-center xl:justify-start" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary transition-all duration-500" />
+            <p className="mt-4">I'm currently an upcoming Senior studying Computer Science <br />at the University of California, Davis.</p>
+            {/* socials */}
+            <div className="mt-4 mb-8 xl:mb-0">
+              <Social
+                containerStyles="flex gap-6 justify-center xl:justify-start"
+                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary transition-all duration-500"
+              />
             </div>
           </div>
-          {/*photo*/}
-          <Photo/>
+          {/* photo */}
+          <div className="ml-4 xl:ml-8">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Home
+export default Home;
