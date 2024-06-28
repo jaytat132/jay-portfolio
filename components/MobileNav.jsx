b07824ab4c677e2bd9d6cbceb1fe63ca4e81b0d9
Jay <jaytat132@gmail.com>
2024-06-28 02:38:35 -0700

    added scroll to mobile sheetview

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -29,7 +29,7 @@ const MobileNav = () => {
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuBurger className="text-[32px] text-accent" />
       </SheetTrigger>
-      <SheetContent className="flex flex-col">
+      <SheetContent className="flex flex-col overflow-y-auto max-h-screen">
         {/* logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
